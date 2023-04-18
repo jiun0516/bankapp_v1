81990ee493adeeae2ada8331efe2550bc321e9c5
jiun0516 <leejiun0516@gmail.com>
2023-04-18 15:33:16 +0900

    로그아웃 기능 구현과 로그인 구현 및 세션 처리

--- src/main/java/com/tenco/bank/controller/AccountController.java
+++ src/main/java/com/tenco/bank/controller/AccountController.java
@@ -1,27 +1,23 @@
 package com.tenco.bank.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.tenco.bank.handler.exception.CustomPageException;
 import com.tenco.bank.handler.exception.CustomRestfullException;
+import com.tenco.bank.handler.exception.UnAuthorizedException;
+import com.tenco.bank.repository.model.User;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	@Autowired
+	private HttpSession session;
 	
-	// todo 
-	// 계좌 목록 페이지
-	// 입금 페이지
-	// 출금 페이지
-	// 이체 페이지
-	// 계좌 상세보기 페이지
-	// 계좌 생성 페이지
-	
-	// http://localhost:8080/account/list
-	// http://localhost:8080/account/
 	/**
 	 * 
 	 * 계좌 목록 페이지
@@ -29,12 +25,14 @@ public class AccountController {
 	 */
 	@GetMapping({"/list", "/"})
 	public String list() {
+
+		// 인증검사 처리
+		User principal = (User)session.getAttribute("principal");
+		if(principal == null) {
+			throw new UnAuthorizedException("인증된 사용자가 아닙니다", HttpStatus.UNAUTHORIZED);
+		}
 		
-		// todo 예외 클래스 - 삭제 예정
-		// throw new CustomRestfullException("인증되지 않은 사용자 입니다.", HttpStatus.UNAUTHORIZED);
-		throw new CustomPageException("페이지를 찾을 수 없습니다", HttpStatus.NOT_FOUND);
-		
-		// return "/account/list";
+		return "/account/list";
 	}
 	
 	// 출금 페이지
